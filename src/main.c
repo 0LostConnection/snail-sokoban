0b004f1c354f237f3ac8646b2abf8e15176a4a88
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-11-09 09:21:57 -0300

    Iniciando

---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main() {
+    printf("Tem que fazer ainda... :)");
+}
---
+++ testes/sokoban.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main() {
+    printf("Pode apagar esse arquivo, ele é só serve para a pasta 'testes' subir pro repositório remoto.");
+}
