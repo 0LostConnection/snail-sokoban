81353521760a36ed7978af9008c193a9e7e589d2
Geovane Saraiva da Silva <70657835+0LostConnection@users.noreply.github.com>
2024-11-12 10:42:31 -0300

    Cleaning

--- src/input.c
+++
@@ -1,9 +0,0 @@
-#include "input.h"
-#include <stdio.h>
-
-char getUserInput() {
-    char input;
-    printf("Use W (cima), A (esquerda), S (baixo), D (direita) para mover: ");
-    scanf(" %c", &input);
-    return input;
-}
--- tests/sokoban.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void main() {
-    printf("Pode apagar esse arquivo, ele é só serve para a pasta 'testes' subir pro repositório remoto.");
-}
